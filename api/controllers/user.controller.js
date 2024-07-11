3c0f1ffc25493c93a88398b409caa80a0bcb82be
AtiqurRahmanAni <56642339+AtiqurRahmanAni@users.noreply.github.com>
2024-07-11 08:53:34 +0600

    delete issue fixed

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,15 +1,13 @@
-import User from "../models/user.model.js"
-import jwt from "jsonwebtoken"
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 import { verifyToken } from "../middlewares/jwt.js";
 
-export const deleteUser = async(req,res) => {
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
 
-    const user = await User.findById(req.params.id);
-
-    
-        if(req.userId != user._id.toString()) {
-            return res.status(403).send("You can delete only your account!");
-        }
-        await User.findByIdAndDelete (req.params.id);
-        res.status(200).send("Deleted.")
-    };
+  if (req.userId != user._id.toString()) {
+    return res.status(403).send("You can delete only your account!");
+  }
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send("Deleted.");
+};
