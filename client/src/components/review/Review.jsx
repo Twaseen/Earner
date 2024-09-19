3fc8d85e044089add119964c5355cef9c69bd239
Ishrat Jahan Mim <141272899+mimjk@users.noreply.github.com>
2024-09-19 12:33:08 +0600

    Mutation,Orders,Messages

--- client/src/components/review/Review.jsx
+++ client/src/components/review/Review.jsx
@@ -1,50 +1,50 @@
-// import { useQuery } from "@tanstack/react-query";
-// import React from "react";
-// import newRequest from "../../utils/newRequest";
-// import "./Review.scss";
-// const Review = ({ review }) => {
-//   const { isLoading, error, data } = useQuery({
-//     queryKey: [review.userId],
-//     queryFn: () =>
-//       newRequest.get(`/users/${review.userId}`).then((res) => {
-//         return res.data;
-//       }),
-//   });
+ import { useQuery } from "@tanstack/react-query";
+ import React from "react";
+ import newRequest from "../../utils/newRequest";
+ import "./Review.scss";
+ const Review = ({ review }) => {
+   const { isLoading, error, data } = useQuery({
+     queryKey: [review.userId],
+     queryFn: () =>
+       newRequest.get(`/users/${review.userId}`).then((res) => {
+         return res.data;
+       }),
+   });
 
-//   return (
-//     <div className="review">
-//       {isLoading ? (
-//         "loading"
-//       ) : error ? (
-//         "error"
-//       ) : (
-//         <div className="user">
-//           <img className="pp" src={data.img || "/img/noavatar.jpg"} alt="" />
-//           <div className="info">
-//             <span>{data.username}</span>
-//             <div className="country">
-//               <span>{data.country}</span>
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//       <div className="stars">
-//         {Array(review.star)
-//           .fill()
-//           .map((item, i) => (
-//             <img src="/img/star.png" alt="" key={i} />
-//           ))}
-//         <span>{review.star}</span>
-//       </div>
-//       <p>{review.desc}</p>
-//     </div>
-//   );
-// };
+   return (
+     <div className="review">
+       {isLoading ? (
+         "loading"
+       ) : error ? (
+         "error"
+       ) : (
+         <div className="user">
+           <img className="pp" src={data.img || "/img/noavatar.jpg"} alt="" />
+           <div className="info">
+             <span>{data.username}</span>
+             <div className="country">
+               <span>{data.country}</span>
+             </div>
+           </div>
+         </div>
+       )}
+       <div className="stars">
+         {Array(review.star)
+           .fill()
+           .map((item, i) => (
+             <img src="/img/star.png" alt="" key={i} />
+           ))}
+         <span>{review.star}</span>
+       </div>
+       <p>{review.desc}</p>
+     </div>
+   );
+ };
 
-// export default Review;
+ export default Review;
 
 
-import React from "react";
+/*import React from "react";
 import "./Review.scss";
 
 const Review = ({ review }) => {
@@ -70,4 +70,4 @@ const Review = ({ review }) => {
   );
 };
 
-export default Review;
+export default Review;*/
