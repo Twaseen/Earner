6e6b6478100a23d0420138ebc44bd4f9ce64632d
Twaseen47 <140600557+Twaseen@users.noreply.github.com>
2024-09-20 13:08:30 +0600

    Payment(need to fix a bit)

--- client/src/pages/orders/Orders.jsx
+++ client/src/pages/orders/Orders.jsx
@@ -2,36 +2,31 @@ import "./Orders.scss";
 
 import { Link, useNavigate } from "react-router-dom";
 import React from "react";
-import{useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
 
-const Orders = ()=> {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+const Orders = () => {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-    const navigate = useNavigate();
-    const { isLoading, error, data } = useQuery({
-      queryKey: ["orders"],
-      queryFn: () =>
-        newRequest
-          .get(
-            `/orders`
-          )
-          .then((res) => {
-            return res.data;
-          }),
-    });
+  const navigate = useNavigate();
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["orders"],
+    queryFn: () =>
+      newRequest.get(`/orders`).then((res) => {
+        return res.data;
+      }),
+  });
 
-  const handleContact = async (order)=>{
+  const handleContact = async (order) => {
     const sellerId = order.sellerId;
     const buyerId = order.buyerId;
     const id = sellerId + buyerId;
 
-    try{
-
+    try {
       const res = await newRequest.get(`/conversations/single/${id}`);
       navigate(`/message/${res.data.id}`);
-    }catch(err){
-      if(err.response.status === 404){
+    } catch (err) {
+      if (err.response.status === 404) {
         const res = await newRequest.post(`/conversations/`, {
           to: currentUser.seller ? buyerId : sellerId,
         });
@@ -39,43 +34,48 @@ const Orders = ()=> {
       }
     }
   };
-  
+
   return (
     <div className="orders">
       {isLoading ? (
-         "loading"
-       ): error ? (
-         "error" 
+        "loading"
+      ) : error ? (
+        "error"
       ) : (
-      <div className="container">
-        <div className="title">
-          <h1>Orders</h1> 
+        <div className="container">
+          <div className="title">
+            <h1>Orders</h1>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Contact</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((order) => (
+                <tr key={order._id}>
+                  <td>
+                    <img className="image" src={order.img} alt="" />
+                  </td>
+                  <td>{order.title}</td>
+                  <td>{order.price}</td>
+                  <td>
+                    <img
+                      className="message"
+                      src="./img/message.png"
+                      alt=""
+                      onClick={() => handleContact(order)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <table>
-          <tr>
-          <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Contact</th>
-          </tr>
-          {data.map((order)=>(
-          <tr key={order._id}>
-            <td>
-              <img
-                className="image"
-                src={order.img}
-                alt=""
-              />
-            </td>
-            <td>{order.title}</td>
-            <td>{order.price}</td>
-            <td>
-              <img className="message" src="./img/message.png" alt="" onClick={()=>handleContact(order)}/>
-            </td>
-          </tr>
-         ))}
-        </table>
-      </div>
       )}
     </div>
   );
--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         // additionalData: @import "src/styles/variables.scss"; // Removed
-      }
-    }
+      },
+    },
   }
 });
